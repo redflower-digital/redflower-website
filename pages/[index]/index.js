fdc46948ff048ba67df03d17deca872788109dbb
adadofr <thioff1@gmail.com>
2022-06-28 02:53:37 +0200

    delete canonical tag

--- pages/[index]/index.js
+++ pages/[index]/index.js
@@ -25,13 +25,9 @@ export default function Index({city}) {
           property="og:description"
           content="Votre entreprise de débarras en Auvergne-Rhône-Alpes"
         />
+        <meta property="og:url" content="https://redflower-debarras.fr/" />
         <meta property="og:type" content="website" />
         <link rel="icon" href="../../favicon.ico" />
-        <link
-          rel="canonical"
-          href="https://www.redflower-debarras.fr/"
-          title="Débarras maison, appartement gratuit à Lyon (69) Rhône-Alpes"
-        />
         <meta name="geo.placename" content="Lyon" />
         <meta name="msnbot" content="index,follow" />
         <meta name="ICBM" content="45.78498101131291, 4.7487209759413815" />
