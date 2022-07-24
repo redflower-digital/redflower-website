6eb0ea5410cc2a7c8042345e33751c48c20d9164
adadofr <thioff1@gmail.com>
2022-07-24 16:14:44 +0200

    alt text modification

--- pages/index.js
+++ pages/index.js
@@ -72,7 +72,7 @@ export default function Index(props) {
   
   Meta.defaultProps = {
     title:
-      "Débarras appartement maison & Rachat de biens | devis gratuit",
+      "Débarras appartement, maison & Rachat de biens | devis gratuit",
     keywords:
       "débarras, entreprise, appartement, maison, enlèvement, biens, gratuit, rachat, valorisation, diogène, syllogomanie, succession, maison, garage, professionnel, monte-meuble, déménagement, Lyon, 69, 42, saint-etienne, Rhône, Auvergne-Rhône-Alpes, bourgogne",
     description:
